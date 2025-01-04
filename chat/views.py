4bca5e37a05b8363dd0a6a4132c33abb86655dcb
Prashant Vaidik <prashant.vaidik@gmail.com>
2025-01-04 12:49:01 -0600

    first commit

---
+++ chat/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+
+def index(request):
+    return render(request, 'chat/index.html', {})
+
+def room(request, room_name):
+    print('--------0000000--------' + room_name)
+    return render(request, 'chat/chatroom.html', {
+        'room_name':room_name
+    })
