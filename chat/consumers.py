1d00570295ed21dfc96c6a7e3b4c38eacaf1cc31
Prashant Vaidik <prashant.vaidik@gmail.com>
2025-01-05 13:33:28 -0600

    url error handling

--- chat/consumers.py
+++ chat/consumers.py
@@ -1,10 +1,25 @@
 from channels.generic.websocket import AsyncWebsocketConsumer
 import json
+import re
 
 class ChatRoomConsumer(AsyncWebsocketConsumer):
     async def connect(self):
         self.room_name = self.scope['url_route']['kwargs']['room_name']
-        self.room_group_name = 'chat_%s' % self.room_name
+
+        # validate room name
+        if not re.match(r'^[a-zA-Z0-9_\-@#\.]+$', self.room_name):
+            self.room_group_name = 'error_chatroom'
+            await self.accept()
+            await self.send(json.dumps({
+                'type':'error',
+                'message': 'Invalid room name!'
+            }))
+
+            await self.close()
+            return
+        
+        else:
+            self.room_group_name = 'chat_%s' % self.room_name
 
         await self.channel_layer.group_add(
             self.room_group_name,
